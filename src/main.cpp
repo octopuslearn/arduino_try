f7d3592bfe1038f4df23932ae5ac345c16d29f00
LAPTOP-O7QRECAP\zy <3148262242@qq.com>
2022-12-08 17:01:40 +0800

    first_try-1.0

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <Arduino.h>
+
+void setup() {
+  pinMode(13,OUTPUT);// put your setup code here, to run once:
+}
+
+void loop() {
+  digitalWrite(13,LOW);// put your main code here, to run repeatedly:
+}
