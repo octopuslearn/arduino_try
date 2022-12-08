e609dd32c3f05a86f1c93c2c550ba87afdb47a38
LAPTOP-O7QRECAP\zy <3148262242@qq.com>
2022-12-08 17:43:29 +0800

    ffff

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@ void setup() {
 
 void loop() {
   digitalWrite(13,LOW);// put your main code here, to run repeatedly:
-  delay(1000);
-  digitalWrite(13,HIGH);
-  delay(1000);
-  digitalWrite(1,LOW);//ceshi
+  delay(10000);//asfaf
+  digitalWrite(13,HIGH);//sfsf
+  delay(1000);//afjao
+  digitalWrite(1,LOW);//ceshi fuck badkjfioajjfazczzc
 }
