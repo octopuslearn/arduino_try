6b2b2835af0498161af95780b4ded97306b36272
LAPTOP-O7QRECAP\zy <3148262242@qq.com>
2022-12-08 17:24:16 +0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,13 @@
 
 void setup() {
   pinMode(13,OUTPUT);// put your setup code here, to run once:
+  pinMode(1,OUTPUT);
 }
 
 void loop() {
   digitalWrite(13,LOW);// put your main code here, to run repeatedly:
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(1,LOW);
 }
