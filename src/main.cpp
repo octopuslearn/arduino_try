ae39ba7d443cffc598909ef07905d5cce7da0008
LAPTOP-O7QRECAP\zy <3148262242@qq.com>
2022-12-09 22:32:08 +0800

    first_start1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,7 @@
 #include <Arduino.h>
 
 void setup() {
-  pinMode(13,OUTPUT);
-  pinMode(1,OUTPUT);
 }
 
 void loop() {
-  digitalWrite(13,LOW);
-  delay(10000);
-  digitalWrite(13,HIGH);
-  delay(1000);
-  digitalWrite(1,LOW);
 }
