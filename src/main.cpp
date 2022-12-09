3662bcf4fa7e711e057f31a16069a31e7634280c
LAPTOP-O7QRECAP\zy <3148262242@qq.com>
2022-12-09 22:29:51 +0800

    first1.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 
 void setup() {
-  pinMode(13,OUTPUT);// put your setup code here, to run once:
+  pinMode(13,OUTPUT);
   pinMode(1,OUTPUT);
 }
 
 void loop() {
-  digitalWrite(13,LOW);// put your main code here, to run repeatedly:
-  delay(10000);//asfaf
-  digitalWrite(13,HIGH);//sfsf
-  delay(1000);//afjao
-  digitalWrite(1,LOW);//ceshi fuck badkjfioajjfazczzc
+  digitalWrite(13,LOW);
+  delay(10000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(1,LOW);
 }
