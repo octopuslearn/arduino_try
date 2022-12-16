0b2ce8d4d01dc7439ef7b1d16b3b89686828d368
LAPTOP-O7QRECAP\zy <3148262242@qq.com>
2022-12-16 12:29:12 +0800

    success

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
                   #include <string.h>
                   #define IBUS_BUFFSIZE 32 ////32个数据   
-                  #define IBUS_MAXCHANNELS 10 // I am using only 10 channels because my TX (FlySky i6) supports max 10 channels//我只使用 10 个通道，因为我的 TX (FlySky i6) 最多支持 10 个通道
-                  #include <Server.h>
+                  #define IBUS_MAXCHANNELS 10 // I am using only 10 channels because my TX (FlySky i6) supports max 10 channels//我只使用 10 个通道，因为我的 TX (FlySky i6) 最多支持 10 个通道 
+                  #include <Servo.h>
 
                   static uint8_t ibusIndex = 0;//第几个数据
                   static uint8_t ibus[IBUS_BUFFSIZE] = {0};//存放32个数据的数组
@@ -34,36 +34,32 @@
                   void setup()
                   {
                     Serial.begin(115200);
-                    // ch1.attach(2);//电机接到的引脚
-                    ch1.attach(7);
-                    // ch3.attach(4);
-                    ch2.attach(5);
-                    ch3.attach(6);
-                    // ch6.attach(7);
-                    // ch7.attach(8);
-                    ch4.attach(9);
-                    ch5.attach(10);
-                    ch6.attach(11);
-                    pinMode(13,OUTPUT);
+                    ch1.attach(2);//PWM输出连接到的引脚
+                    ch2.attach(3);
+                    ch3.attach(4);
+                    ch4.attach(5);
+                    ch5.attach(6);
+                    ch6.attach(7);
+                    ch7.attach(8);
+                    ch8.attach(9);
+                    ch9.attach(10);
+                    ch10.attach(11);
                   }
                   
                   void loop()
                   {
                     readRx();
+                    
                   }
                   
                   void readRx()
                   {
 
-                    rxFrameDone = false;//flase-0,true-1(即真假)
+                    // rxFrameDone = false;//flase-0,true-1(即真假) //可以作为判断一整个数据有没有解析完的依据，是有用的
                    
                     if (Serial.available())//返回串口缓冲区中当前剩余的字符个数。一般用这个函数来判断串口的缓冲区有无数据，当Serial.available()>0时，说明串口接收到了数据，可以读取；
                                            //Serial.read()指从串口的缓冲区取出并读取一个Byte的数据，比如有设备通过串口向Arduino发送数据了，我们就可以用Serial.read()来读取发送的数据。
                     {
-                    digitalWrite(13,HIGH);
-                    delay(1000);
-                    digitalWrite(13,LOW);
-                    delay(1000);
                       uint8_t val = Serial.read();////读到的串口数据
                       // Look for 0x2040 as start of packet
                       if (ibusIndex == 0 && val != 0x20)//寻找 0x2040 作为数据包的开始
@@ -82,7 +78,7 @@
                         ibusIndex = 0;
                         int high=3;
                         int low=2;
-                        for(int i=0;i<IBUS_MAXCHANNELS; i++)//左移第一个字节的前 8 位，并加上前一个字节的全部值
+                        for(int i=0;i<IBUS_MAXCHANNELS; i++)//左移第一个字节的前 8 位，并加上前一个字节的全部值?????
                         {
                           rcValue[i] = (ibus[high] << 8) + ibus[low];
                           high += 2;
@@ -133,8 +129,10 @@
                        
                         ch_width_8 = map(rcValue[7], 1000, 2000, 1000, 2000);
                         ch8.writeMicroseconds(ch_width_8);
-                            Serial.print(ch_width_8);
-                            Serial.print("     ");
+                            // Serial.print(ch_width_8);
+                            Serial.print(rcValue[7]);
+                            Serial.print();
+                            
                        
                         ch_width_9 = map(rcValue[8], 1000, 2000, 1000, 2000);
                         ch9.writeMicroseconds(ch_width_9);
@@ -148,17 +146,14 @@
                             Serial.print(ch_width_10);
                             Serial.println("     ");
                                                   
-                        rxFrameDone = true;
-                        return;
+                        // rxFrameDone = true;//见上面解析
+                        // return;//return 后面也可以不跟任何数据，表示什么也不返回，仅仅用来结束函数。
                       }
                       else
                       {
                         ibus[ibusIndex] = val;
                         ibusIndex++;
-                    digitalWrite(13,HIGH);
-                    delay(1000);
-                    digitalWrite(13,LOW);
-                    delay(1000);
                       }
+
                     }
                   }
